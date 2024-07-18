13bf6bf0210fead400070a8556bd5cae8922cfbc
Akhil <akhil1659@gmail.com>
2024-07-18 14:33:38 +0530

    some changes

--- server/dist/app.js
+++ server/dist/app.js
@@ -7,7 +7,11 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 // using routes
 app.use("/", (req, res) => {
     res.send("Welcome to Image Description API");
--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }
+));
 
 // using routes
 app.use("/", (req, res) => {
